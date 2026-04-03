7a191dcfb4b3fe49b54d65067ade04df23b5c107
Benjamin2320 <141332937+Benjamin2320@users.noreply.github.com>
2026-04-03 15:38:29 -0500

    Update main.tf

--- First Terraform Deployment/main.tf
+++ First Terraform Deployment/main.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
     region = "us-east-1"
 }
+
+resource "aws_instance" "example" {
+    ami           = "ami-0c101f26f147fa7fd"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-first-instance"
+    }
+}
